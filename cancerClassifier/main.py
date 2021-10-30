3bfae955415f7b2cde062710ec423ef08bb0c4fb
Koushik Das <sd2021667@gmail.com>
2021-10-31 00:06:35 +0530

    created cancerClassifier

---
+++ cancerClassifier/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib as plt
+import seaborn as sns
+from sklearn import datasets
+from sklearn import preprocessing
+from sklearn import linear_model
+from sklearn import model_selection as ms
+
