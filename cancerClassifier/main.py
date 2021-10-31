c8c756d9ab4c30f2b1a73c72ff156c7b883d5ac9
Koushik Das <sd2021667@gmail.com>
2021-10-31 18:04:49 +0530

    cleaned code

--- cancerClassifier/main.py
+++ cancerClassifier/main.py
@@ -7,16 +7,10 @@ from sklearn import linear_model
 from sklearn import model_selection as ms
 
 
-# In[6]:
-
-
 cancer_data = datasets.load_breast_cancer()
 y = cancer_data.target # Training labels ('malignant = 0', 'benign = 1')
 X = cancer_data.data # 30 attributes; https://scikit-learn.org/stable/datasets/index.html#breast-cancer-dataset
 X = preprocessing.scale(X) # scale each data attribute to zero-mean and unit variance 
-
-
-# In[24]:
 
 
 # Plot the first 2 attributes of training points
@@ -25,9 +19,6 @@ plt.xlabel('Tumor Radius')
 plt.ylabel('Tumor Texture')
 plt.grid(True)
 plt.show()
-
-
-# In[30]:
 
 
 alpha = np.arange(1e-15,1,0.005) # Range of hyperparameter values 1E-15 to 1 by 0.005
@@ -41,9 +32,6 @@ for i in range(len(alpha)): # for each alpha value
     val_scores[i] = score.mean() # Calculate mean of the 5 scores
 
 
-# In[31]:
-
-
 # Plot how cross-validation score changes with alpha
 plt.plot(alpha,val_scores)
 plt.xlim(0,1)
@@ -51,9 +39,6 @@ plt.xlabel('alpha')
 plt.ylabel('Mean Cross-Validation Accuracy')
 plt.grid(True)
 plt.show()
-
-
-# In[47]:
 
 
 # Determine the alpha that maximizes the cross-validation score
@@ -71,17 +56,11 @@ plt.grid(True)
 plt.show()
 
 
-# In[67]:
-
-
 # Train model with alpha_star
 model_star = linear_model.SGDClassifier(loss='hinge', penalty='l2', alpha=alpha_star)
 model_trained = model_star.fit(X,y)
 print('Training Accuracy =', model_trained.score(X,y))
 # Training Accuracy = 0.9806678383128296
-
-
-# In[62]:
 
 
 # Plot decision boundary of trained model
